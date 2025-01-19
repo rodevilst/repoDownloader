9aa6b2f5154de60ac4cfe841a174dcb8a6c5c2e6
rodevilst <97972449+rodevilst@users.noreply.github.com>
2025-01-19 12:03:18 +0200

    start project

---
+++ src/main/java/com/forgeflow/repodownloader/RepoDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.forgeflow.repodownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoDownloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forgeflow/repodownloader/RepoDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forgeflow.repodownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
